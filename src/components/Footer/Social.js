f0c115df7ace7a5c88a8e34c788c562d3da26108
Kirkemann <kirkemann35@gmail.com>
2019-10-03 10:11:27 +0200

    slut på opgaven

--- src/components/Footer/Social.js
+++ src/components/Footer/Social.js
@@ -5,9 +5,9 @@ const Social = () => {
             <div className="p-5">
             <h6 className="text-white">SOCIALE MEDIER - FØLG OS HER</h6> 
             <ul className="list-unstyled text-white">
-                <li>Facebook <i class="fab fa-facebook-square"></i></li>
-                <li>Instagram <i class="fab fa-instagram"></i></li>
-                <li>Linkedin <i class="fab fa-linkedin"></i></li>
+                <li>Facebook <i className="fab fa-facebook-square"></i></li>
+                <li>Instagram <i className="fab fa-instagram"></i></li>
+                <li>Linkedin <i className="fab fa-linkedin"></i></li>
                 
             </ul>
         </div>
--- src/components/Underside/OmOS.js
+++ src/components/Underside/OmOS.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import KontaktOS from './KontaktOs'
-import bill from '../img/ScubaLiving_logo.svg'
+import React, { useState } from 'react';
 
 
-const OmOs = () => {
+
+const OmOS = () => {
+
+    const [dykker]= useState([
+       
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker joe', image: 'dykker_1.jpg', name: 'Joe joeson', what: 'Her kommer tekst om dykkeren......',  id: 1},
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker jullie', image: 'dykker_2.jpg', name: 'Jullie julson', what:'Her kommer tekst om dykkeren......', id: 2},
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker dave', image: 'dykker_3.jpg', name: 'Jullie julson', what: 'Her kommer tekst om dykkeren......' , id: 3},
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker hans', image: 'dykker_4.jpg', name: 'Hans Hanson', what: 'Her kommer tekst om dykkeren......' , id: 4},
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker mette', image: 'dykker_5.jpg', name: 'Mette Metteson', what: 'Her kommer tekst om dykkeren......' , id: 5},
+        {class: 'col-12 col-lg-5 col-md-6 card p-5 m-5', alt: 'Vores dykker don', image: 'dykker_6.jpg', name: 'Don Donson', what: 'Her kommer tekst om dykkeren......' , id: 6}
+       
+    ]);
+
     return ( 
-        <div className="container">
-            <div className="row">
-            <div className="col-12 col-lg-6">
-                <KontaktOS />
-            </div>
-            <div className="col-12 col-lg-6">
-                <img className="img-fluid bill" src={bill} alt=""/>
-            </div>
-            </div>
-        </div>
+
+                    <div className="row mt-5 ml-5">
+                    {dykker.map(d => {
+                        return ( 
+                            <div key={d.id} className={d.class}>
+                                <img className="img-fluid card-img-top" src={"/images/" + d.image} alt={d.alt} />
+                                <div>
+                                    <h4 className="card-title text-center"> {d.name} </h4>
+                                    <p className="card-text text-center">{d.what} </p>
+                                </div>
+                            </div>
+                         );
+                    })}
+
+                    </div>
+
+                    
      );
 }
- 
-export default OmOs;
+
+export default OmOS;
